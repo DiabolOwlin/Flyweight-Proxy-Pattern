ff11f94a9db02bb481dd21e4c8092fcc441a806e
DiabolOwlin <DiabolOwlin@users.noreply.github.com>
2021-12-13 17:35:36 +0100

    Geographic coordinates were added, refactoring completed, next step is adding a tkinter GUI

--- Flyweight.py
+++ Flyweight.py
@@ -1,44 +1,68 @@
+import random
 from Proxy import *
+
 
 class Surname(object):
     def __init__(self, surname):
         self.surname = surname
 
 
-class PersonFactory:
-    people = {}
+class SurnameFactory:
+    _surname: dict[str, Surname] = {}
 
     @staticmethod
     def get_person(surname):
-        return PersonFactory.people.setdefault(surname, Surname(surname))
+        return SurnameFactory._surname.setdefault(surname, Surname(surname))
+
+    @classmethod
+    def show_surname_flyweights(cls):
+        print("==============================================================")
+        print("Surnames registered in database(surname flyweights):\n[" + " ".join(map(str, cls._surname.keys())), end="]\n")
 
 
-class NameBranch(object):
+class Name(object):
     def __init__(self, name):
         self.name = name
 
     def add_person_to_dictionary(self, surname):
-        print("Add {} surname with id [{}] to name dictionary {} with id [{}]".format(surname.surname, id(surname), self.name, id(self)))
+        # print("Add {} surname with id [{}] to name dictionary {} with id [{}]".format(surname.surname, id(surname), self.name, id(self)))
+        return surname.surname, id(surname), self.name, id(self)
 
 
 class Database(object):
+    _names: dict[str, Name] = {}
+
     def __init__(self):
+        self.persons: dict[tuple, tuple] = {}
         self.people_count = 0
-        self.names = {}
 
     def get_name(self, name):
-        return self.names.setdefault(name, NameBranch(name))
+        return self._names.setdefault(name, Name(name))
 
     def add_to_the_database(self, name, surname):
         tmp = ProxyPerson()
         checked_name, checked_surname = tmp.check_spelling(name, surname)
-        self.get_name(checked_name).add_person_to_dictionary(PersonFactory.get_person(checked_surname))
-        self.people_count += 1
+        added_surname, surname_id, added_name, name_id = self.get_name(checked_name)\
+            .add_person_to_dictionary(SurnameFactory.get_person(checked_surname))
+
+        if (added_name, added_surname) not in self.persons.keys():
+            self.persons.setdefault((added_name, added_surname), (random.uniform(51.5, 53.95), random.uniform(23.15, 23.95)))
+            print("Add '{}' surname with id [{}] to name dictionary '{}' with id [{}]".format(added_surname, surname_id, added_name, name_id))
+            self.people_count += 1
+        else:
+            print(f"Person with the name '{added_name}' and surname '{added_surname}' already exists in base,"
+                  f" was detained at the coordinates of "
+                  f"{self.persons[(added_name, added_surname)][0]},{self.persons[(added_name, added_surname)][1]}")
+
+        # print("Person dict length: ", len(self.persons))
+        # print("Surname dict length:", self.get_name(checked_name).add_person_to_dictionary)
+
+    def show_name_flyweights(self):
+        print("==============================================================")
+        print("Names registered in database(name_flyweights):\n["+" ".join(map(str, self._names.keys())), end=']\n')
 
 
-# if __name__ == '__main__':
-#     registered_people = Database()
-#     registered_people.add_to_the_database("Banyk", "Andrii")
-#     registered_people.add_to_the_database("Sizomin", "Bohdan")
-#     registered_people.add_to_the_database("Sizomin", "Andrii")
-#     registered_people.add_to_the_database("Alexey", "Plakhin")
+# 53.93676763666636, 23.573060900509603
+# 52.74302534181399, 23.92462340294286
+# 52.234611402168106, 23.177553085272187
+# 51.54300323270001, 23.59503355691168
--- main.py
+++ main.py
@@ -6,3 +6,12 @@ if __name__ == '__main__':
      person_database.add_to_the_database("mAx", "BanYK")
      person_database.add_to_the_database("Arsenii", "Banyk")
      person_database.add_to_the_database("andrII", "Sizomin")
+     person_database.add_to_the_database("andrII", "Sizomin")
+     person_database.add_to_the_database("Maria", "Skladowska")
+     person_database.add_to_the_database("PatI", "SklAdowska")
+     person_database.add_to_the_database("MaX", "Bujalski")
+     person_database.add_to_the_database("mAx", "Bujalski")
+
+
+     person_database.show_name_flyweights()
+     SurnameFactory.show_surname_flyweights()
